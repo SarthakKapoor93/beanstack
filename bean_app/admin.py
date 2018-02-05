d1ba1c8c8c520502bbc5f3c70024573aae4645f8
Sarthak Kapoor <2349677K@student.gla.ac.uk>
2018-02-05 12:23:36 +0000

    Added UserAccount model, modified Admin file and made migrations

--- bean_app/admin.py
+++ bean_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from bean_app.models import CoffeeProduct, ProductReview
+from bean_app.models import CoffeeProduct, ProductReview, UserAccount
 
 admin.site.register(CoffeeProduct)
 admin.site.register(ProductReview)
+admin.site.register(UserAccount)
--- bean_app/models.py
+++ bean_app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class CoffeeProduct(models.Model):
-
     name = models.CharField(max_length=128, unique=True)
     description = models.CharField(max_length=1000, blank=True)
     origin = models.CharField(max_length=128)
@@ -14,7 +14,6 @@ class CoffeeProduct(models.Model):
 
 
 class ProductReview(models.Model):
-
     coffee_product = models.ForeignKey(CoffeeProduct)
     description = models.CharField(max_length=240)
     rating = models.IntegerField(default=0)
@@ -22,6 +21,15 @@ class ProductReview(models.Model):
 
     def __str__(self):
         return "Review of {} by {}".format(self.coffee_product, self.reviewer)
+
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(User)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 '''
