c7ee7b0b9309cdba9d3fd723aef926584a06a9c5
Stuart Miller <0404322M@student.gla.ac.uk>
2018-02-15 11:30:52 +0000

    minor changes

--- bean_app/urls.py
+++ bean_app/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^signup', views.signup, name='signup'),
     url(r'^vendorsignup', views.signup, name='vendorsignup'),
     url(r'^signupselection', views.signupselection, name='signupselection'),
-    url(r'^maps', views.maps, name='maps'),
+    url(r'^maps$', views.maps, name='maps'),
     url(r'^load_api', views.load_api, name='load_api')
     ]
 
