d828fcdef2f512d3aeceb3cd1c283b75aa6e2c2c
Stuart Miller <0404322M@student.gla.ac.uk>
2018-03-10 09:48:38 +0000

    Added an extra url catcher so that the home page can also be accessed at localhost:8000/bean_app/

--- bean_app/urls.py
+++ bean_app/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from bean_app import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^home', views.home, name='home'),
     url(r'^about', views.about, name='about'),
     url(r'^contact', views.contact, name='contact'),
