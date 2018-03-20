e8be55b8e55d22a78ab2efcea5242ab0618295fd
Stuart Miller <0404322M@student.gla.ac.uk>
2018-03-20 10:45:18 +0000

    Made sure that the images for the coffees are available after creating a new database

--- bean_app/templatetags/bean_app_template_tags.py
+++ bean_app/templatetags/bean_app_template_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from beanstack.settings import MEDIA_ROOT
+from pathlib import Path
 
 register = template.Library()
 
@@ -9,3 +11,9 @@ def get_stars(rating, colour):
     for star in range(0, int(rating)):
         s += '<span class="glyphicon glyphicon-star" style="color: ' + colour + '; font-size: 20px;"></span>'
     return s
+
+
+@register.filter(name='image_exists')
+def image_exists(image_name):
+    return Path(MEDIA_ROOT + '/' + image_name + '.jpeg').exists()
+
